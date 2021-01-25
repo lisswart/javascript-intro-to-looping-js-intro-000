30ea0e0fbee5dd3500fa59724b652fe756ac6371
Lisa Swart <arnoudlisa@gmail.com>
2021-01-25 09:52:39 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,9 @@
-function forLoop(let i = 0; i < 25; i++) {
-  if (i === 1) {
-    console.log(`I am ${i} strange loop.`)
-  } else {
-    console.log(`I am ${i} strange loops.`)
-  }
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`I am ${i} strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+    return array
 }
