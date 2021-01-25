70ce23b36b4d5c7cd5cb55a68111e74ebeec06b4
Lisa Swart <arnoudlisa@gmail.com>
2021-01-25 09:48:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,30 +1,7 @@
-function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push(`I am ${i} strange loop.`)
-    } else {
-      array.push(`I am ${i} strange loops.`)
-    }
+function forLoop(let i = 0; i < 25; i++) {
+  if (i === 1) {
+    console.log(`I am ${i} strange loop.`)
+  } else {
+    console.log(`I am ${i} strange loops.`)
   }
-  return array
-}
-
-function whileLoop(number)  {
-  while (number > 0) {
-    console.log(--number)
-  }
-  return "done"
-}
-
-function doWhileLoop(num) {
-  var i = 0;
-
-  function incrementVariable() {
-          i = i + 1;
-          return i;
-        }
-
-  do {
-    console.log("I run once regardless.")
-  } while (incrementVariable() < num)
 }
